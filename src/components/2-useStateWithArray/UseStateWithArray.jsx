9738e972b245822602a1949c986c14d0286f56ff
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2023-07-01 12:21:06 +0100

    add delete functionality

--- src/components/2-useStateWithArray/UseStateWithArray.jsx
+++ src/components/2-useStateWithArray/UseStateWithArray.jsx
@@ -1,28 +1,43 @@
 import { useState } from "react";
 import employees from "../../data";
-import "./style.css"
-
+import "./style.css";
 
 const UseStateWithArray = () => {
-
   const [staff, setStaff] = useState(employees);
   // console.log(staff)
+
+  const removeItem = (id) => {
+    // logic
+    // console.log(id)
+    const newEmployee = staff.filter((person) => person.id !== id);
+    // console.log(newEmployee);
+    setStaff(newEmployee);
+  };
+
+  const clearAllItems = () => {
+    setStaff([]);
+  };
 
   return (
     <div>
       <h1>UseState Examples with arrays</h1>
       <div className="container">
-        {employees.map((user) => {
+        {staff.map((user) => {
+          const { id, name, role, gender } = user;
           return (
-            <div key={user.id} className="content">
-              <h2>Name:{user.name}</h2>
-              <span>Position:{user.role}</span>
-              <h4>Gender: {user.gender}</h4>
+            <div key={id} className="content">
+              <h2>Name:{name}</h2>
+              <span>Position:{role}</span>
+              <h4>Gender: {gender}</h4>
+              <button onClick={() => removeItem(id)} className="btn">
+                Delete
+              </button>
             </div>
           );
         })}
       </div>
+      <button onClick={clearAllItems} className="clear-btn">Clear Items</button>
     </div>
   );
-}
-export default UseStateWithArray
+};
+export default UseStateWithArray;
